ac27b2c2224d71b29369e293a5d28e722eff3ab3
vladyslav-koshulko <koshulkov@gmail.com>
2024-02-04 23:28:55 +0200

    Init plugin configs and dependencies

---
+++ src/main/java/org/example/DependencyReporter.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "report")
+public class DependencyReporter extends AbstractMojo {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        System.out.println("Version reporter init");
+    }
+}
